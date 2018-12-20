87c8a38877c663dc0370a4984f24cecf750b4572
rbw0 <rbw@vault13.org>
2018-12-20 16:35:33 +0100

    Get schemas as List instead of One

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -1,10 +1,9 @@
-import { getOne } from 'containers/Backend/actions';
+import { getList } from 'containers/Backend/actions';
 import { LOAD_REQUESTED, LOAD_RESOLVED, LOAD_REJECTED } from './constants';
 
 export function getRPCSchemas() {
-  return getOne({
-    path: 'api',
-    id: 'node',
+  return getList({
+    path: 'api/node',
     onStart: LOAD_REQUESTED,
     onSuccess: LOAD_RESOLVED,
     onError: LOAD_REJECTED,
